70e190f448b49ce2fc4f4e98633c169e2949ceab
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-25 14:49:24 +0100

    Add: artists migration

---
+++ database/migrations/2022_01_25_134425_create_artists_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 40);
+            $table->string('surname', 40);
+            $table->string('main_genre', 40);
+            $table->foreignId('disc_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artists');
+    }
+}
