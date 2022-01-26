bf25d540d3c19b20dd9aabf79f0bd0b0dfd0c6ce
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-26 11:42:14 +0100

    Add: SongFactory

---
+++ database/factories/SongFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Song;
+use Faker\Generator as Faker;
+
+$factory->define(Song::class, function (Faker $faker) {
+    return [
+        'title' => $faker -> name(),
+        'genre' => $faker -> word(),
+        'duration' => $faker -> time('i_s'),
+        'explicit' => $faker -> boolean(),
+        'single' => $faker -> boolean(),
+        'rating' => $faker -> numberBetween(0, 10),
+        'lyric' => $faker -> text(250) 
+    ];
+});
