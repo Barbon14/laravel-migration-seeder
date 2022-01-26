23027924f9f2418832053e7fdf09a1c5f1662689
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-26 12:19:27 +0100

    Fix: sintax errors

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             SongSeeder::class,
-            AlbumSeeder::class
+            ArtistSeeder::class
         ]);
     }
 }
